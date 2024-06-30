9157c7eedd5970cd7f31e1fba5503c27a80da620
DanhTruc2002 <danhtrucsaison@gmail.com>
2024-06-30 15:12:30 +0700

    add product, category

---
+++ src/main/java/com/ecom/ECommerceMobileApplication.java
@@ -0,0 +1,13 @@
+package com.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceMobileApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ecom/controller/HomeController.java
@@ -0,0 +1,33 @@
+package com.ecom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/signin")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/register")
+	public String register() {
+		return "register";
+	}
+	
+	@GetMapping("/products")
+	public String products() {
+		return "product";
+	}
+	
+	@GetMapping("/product")
+	public String product() {
+		return "view_product";
+	}
+}
