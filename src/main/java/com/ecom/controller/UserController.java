b08aa83ebad06b6a5a5f68d5e8bde89c2a82dc4d
danhtrucnguyen <danhtrucsaison@gmail.com>
2024-07-16 22:13:24 +0700

    product ordering

--- src/main/java/com/ecom/controller/UserController.java
+++ src/main/java/com/ecom/controller/UserController.java
@@ -9,15 +9,18 @@ import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ecom.model.Cart;
 import com.ecom.model.Category;
+import com.ecom.model.OrderRequest;
 import com.ecom.model.UserDtls;
-import com.ecom.repository.UserRepository;
+//import com.ecom.repository.UserRepository;
 import com.ecom.service.CartService;
 import com.ecom.service.CategoryService;
+import com.ecom.service.OrderService;
 import com.ecom.service.UserService;
 
 import jakarta.servlet.http.HttpSession;
@@ -33,6 +36,9 @@ public class UserController {
 	
 	@Autowired
 	private CartService cartService;
+	
+	@Autowired
+	private OrderService orderService;
 
 	@GetMapping("/")
 	public String home() {
@@ -89,4 +95,19 @@ public class UserController {
 		UserDtls userDtls = userService.getUserByEmail(email);
 		return userDtls;
 	}
+	
+	@GetMapping("/orders")
+	public String orderPage() {
+		return "/user/order";
+	}
+
+	@PostMapping("/save-order")
+	public String saveOrder(@ModelAttribute OrderRequest request, Principal p) {
+		// System.out.println(request);
+		UserDtls user = getLoggedInUserDetails(p);
+		orderService.saveOrder(user.getId(), request);
+
+		return "/user/success";
+	}
+
 }
